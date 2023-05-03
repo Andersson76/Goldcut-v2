48a590b0ff7254f68319f3dbd19deebca7849233
Martin Andersson <martin.andersson@me.com>
2023-05-03 10:28:03 +0200

    fix

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -2,9 +2,20 @@
 /**
  * Loads the WordPress environment and template.
  *
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
  * @package WordPress
  */
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
 define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
 
 if ( ! isset( $wp_did_header ) ) {
 
