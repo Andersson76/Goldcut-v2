b84715e4bfc76c8db0aafe6615f3f1e43cf81bef
Martin Andersson <martin.andersson@me.com>
2023-05-03 10:58:40 +0200

    db name

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'goldcut' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
